94b86520f2f59ca2965336445e5d4df4061b9489
swatt45 <swatt45@wgu.edu>
2024-03-14 12:59:21 -0500

    Uploading all changes through Step 1

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -54,42 +54,42 @@ if __name__ == "__main__":
   
     parser.add_argument(
         "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = str,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = str,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = str,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = str,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = float,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
+        type = float,
         help = ## INSERT DESCRIPTION HERE,
         required = True
     )
