a8c32932add62c5c71fbf1ea32367100a909af09
swatt45 <swatt45@wgu.edu>
2024-03-14 21:38:02 -0500

    Step 4 Debugging

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -73,7 +73,7 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)    
     ######################################
 
     # Compute r2 and MAE
@@ -97,7 +97,9 @@ def go(args):
     # HINT: use mlflow.sklearn.save_model
     signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_pipe,
+        "random_forest_dir", 
+        serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
         signature = signature,
         input_example = X_train.iloc[:5]
     )
@@ -121,7 +123,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -164,7 +166,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
+        SimpleImputer(strategy="constant", fill_value=0), 
+        OrdinalEncoder()
     )
     ######################################
 
@@ -227,7 +230,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
+            ("preprocessor", preprocessor),
+            ("classifier", RandomForestClassifier(**model_config["random_forest"])),
         ]
     )
 
